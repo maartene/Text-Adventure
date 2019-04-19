d9c6a6f2752b52a6fb0dbd5e98cdf7e0b569e810
Maarten Engels <maartene@mac.com>
2019-04-19 13:43:18 +0200

    Initial Commit

---
+++ Text Adventure/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Text Adventure
+//
+//  Created by Maarten Engels on 19/04/2019.
+//  Copyright © 2019 thedreamweb. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Text Adventure/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Text Adventure
+//
+//  Created by Maarten Engels on 19/04/2019.
+//  Copyright © 2019 thedreamweb. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
